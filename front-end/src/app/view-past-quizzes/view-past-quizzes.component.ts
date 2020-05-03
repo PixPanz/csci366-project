cec80191357c3917fc4ae2c73080e4ec791814e0
hendricksonjonah27 <58757795+hendricksonjonah27@users.noreply.github.com>
2020-05-03 14:26:19 -0500

    Started on Front-end
    Routing is mostly done. Working on css for the pages

---
+++ front-end/src/app/view-past-quizzes/view-past-quizzes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-past-quizzes',
+  templateUrl: './view-past-quizzes.component.html',
+  styleUrls: ['./view-past-quizzes.component.css']
+})
+export class ViewPastQuizzesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
