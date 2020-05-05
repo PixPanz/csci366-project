14894f4ae9281563fbf667caaee776e95b7cae18
hendricksonjonah27 <58757795+hendricksonjonah27@users.noreply.github.com>
2020-05-05 01:55:13 -0500

    Routing actually working

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { QuizListComponent } from './quiz-list/quiz-list.component';
+import { ViewPastQuizzesComponent } from './view-past-quizzes/view-past-quizzes.component';
+import { QuizCreatorComponent } from './quiz-creator/quiz-creator.component';
+import { DockerComponent } from './docker/docker.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'Quizzes', component: QuizListComponent },
+  { path: 'PastQuizzes', component: ViewPastQuizzesComponent },
+  { path: 'CreateAQuiz', component: QuizCreatorComponent },
+  { path: 'Index', component: DockerComponent },
+  { path: 'Login', component: LoginComponent },
+  { path: 'Register', component: RegisterComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
